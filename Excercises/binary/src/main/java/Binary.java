f2a304cd7ec9d01db6bd0a81b923430921af08e1
thegabriele97 <gabriele.lagreca@gmail.com>
2018-04-08 21:02:08 +0200

    Algorythm implemented. #tests 15/15

---
+++ Excercises/binary/src/main/java/Binary.java
@@ -0,0 +1,36 @@
+
+/**
+ * 
+ * @author Gabriele La Greca
+ * 
+ */
+
+public class Binary {
+    private byte[] number;
+
+    public Binary(String number) {
+        this.number = new byte[number.length()];
+        int index = 0;
+
+        for (char ch : number.toCharArray()) {
+
+            if (!Character.isDigit(ch) || Character.getNumericValue(ch) > 1) {
+                this.number = new byte[1];
+                this.number[0] = 0;
+                break;
+            }
+
+            this.number[index++] = (byte)Character.getNumericValue(ch);
+        }
+    }
+
+    public int getDecimal() {
+        int value = 0;
+        
+        for (int i = 0; i < number.length; i++) {
+            value += (number[i] == 1) ? (int)Math.pow(2, number.length - i - 1) : 0;
+        }
+
+        return value;
+    }
+}
