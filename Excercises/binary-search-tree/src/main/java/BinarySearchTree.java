1b1324895dc265bbab2f18511ddcee282f43e5bf
thegabriele97 <gabriele.lagreca@gmail.com>
2018-04-08 12:08:29 +0200

    getAsSortedList() && getAsLevelOrderList() not implemented yet

--- Excercises/binary-search-tree/src/main/java/BinarySearchTree.java
+++ Excercises/binary-search-tree/src/main/java/BinarySearchTree.java
@@ -1,10 +1,40 @@
 import java.util.List;
 
 class BinarySearchTree<T extends Comparable<T>> {
-    void insert(T value) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+    private Node<T> root;
+
+    /**
+     * constructor of BST
+     */
+    BinarySearchTree() {
+        this.root = null;
     }
 
+    void insert(T value) {
+        Node<T> prev = null;
+        Node<T> newNode = new Node<T>(value);
+
+        if (root == null) {
+            root = newNode;
+            return;
+        }
+
+        Node<T> curr = this.root;
+        while (curr != null) {
+            int cmp = value.compareTo(curr.getData());
+
+            prev = curr;
+            curr = (cmp <= 0) ? curr.left : curr.right;
+        }
+
+        int cmp = value.compareTo(prev.getData());
+        if (cmp <= 0) {
+            prev.left = newNode;
+        } else if (cmp > 0) {
+            prev.right = newNode;
+        }
+    }
+ 
     List<T> getAsSortedList() {
         throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
     }
@@ -14,21 +44,37 @@ class BinarySearchTree<T extends Comparable<T>> {
     }
 
     Node<T> getRoot() {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        return root;
     }
 
     static class Node<T> {
+        private T data;
+        private Node<T> left;
+        private Node<T> right;
+
+        /**
+         * constructor of node
+         */
+        private Node(T data, Node<T> left, Node<T> right) {
+            this.data = data;
+            this.left = left;
+            this.right = right;
+        }
+
+        private Node(T data) {
+            this(data, null, null);
+        }
 
         Node<T> getLeft() {
-            throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+            return left;
         }
 
         Node<T> getRight() {
-            throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+            return right;
         }
 
         T getData() {
-            throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+            return data;
         }
 
     }
